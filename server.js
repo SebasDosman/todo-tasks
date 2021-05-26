7aaff96cab2480fb38d4ef95e7f7afce4151c323
SebasDosman <jsdosman0@gmail.com>
2021-05-26 02:05:12 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const api = require('./api');
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.json());
+
+app.listen(port, function () {
+    console.log("Server is listening at port: " + port);
+});
+
+app.get('/', function (req, res) {
+    res.send("hello world");
+});
+
+app.use('/api', api);
